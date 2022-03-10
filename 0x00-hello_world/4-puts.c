f0bdcf5355f4431936e7d26f441a7edd83455c86
XVI-IX <oladoja14@gmail.com>
2022-03-10 20:14:30 +0000

    Minor adjustments to files 2, 4, 6

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
+/**
+ * main - A function to print text to terminal using puts()
+ *
+ * Return: Prints out specified string
+ */
 
 int main(void)
 {
-	char *t = "Programming is like building a multilingual puzzle";
+	char *t = "\"Programming is like building a multilingual puzzle";
 
 	puts(t);
 
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,10 +8,11 @@ int main(void)
 {
 	char c;
 	int i;
+	float f;
 
-	printf("Size of a char: %c byte\n", sizeof(c));
+	printf("Size of a char: %d byte\n", sizeof(c));
 	printf("Size of an int: %d byte(s)\n", sizeof(i));
 	printf("Size of a long int: %d byte(s)\n", (long)sizeof(i));
 	printf("Size of a long int: %d byte(s)\n", (long long int)sizeof(i));
-	printf("Size of a float: %d byte(s)\n", (float)sizeof(i));
+	printf("Size of a float: %d byte(s)\n", (sizeof(f));
 }
