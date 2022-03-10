5f400e82b1c0c3cd2cc56129cec59fb6473850a3
XVI-IX <oladoja14@gmail.com>
2022-03-10 20:50:08 +0000

    Advanced tasks

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - A function to print to stderr
+ *
+ * Return: returns 1
+ */
+
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\"
+	- Dora Korpar, 2015-10-19");
+}
