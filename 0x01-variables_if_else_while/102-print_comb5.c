2445b5a6b135e0092039d99646645d85849b9344
XVI-IX <oladoja14@gmail.com>
2022-03-11 13:18:15 +0000

    Adjusted file 4

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ *
+ *
+ */
+int main(void)
+{
+	int i, j, k;
+
+	i = 100;
+
+	for(; i < 100; )
+	{
+		k = i / 10;
+		j = i % 10;
+
+		putchar(k);
+		putchar(j);
+		
+		if (i < 100)
+		{
+			putchar
+		}
+	}
+}
