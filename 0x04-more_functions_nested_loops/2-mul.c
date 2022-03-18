203d97f6b474ea0de7ea667a01689b22bfef583a
XVI-IX <oladoja14@gmail.com>
2022-03-18 22:15:12 +0000

    Added 2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * mul - multiplies two integers
+ *
+ * Returns - product of multiplication
+ *
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,3 +2,4 @@
 
 int _isupper(int c);
 int __isdigit(int c);
+int mul(int a, int b);
