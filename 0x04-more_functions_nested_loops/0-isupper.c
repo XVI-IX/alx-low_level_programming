f4a17f5903f865136431fbdf15a64667f0e5b73a
XVI-IX <oladoja14@gmail.com>
2022-03-18 22:05:22 +0000

    Add 0x04

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _isupper - Checks if character inputted is uppercase
+ *
+ * Return: Returns 0
+ */
+int _isupper(int c)
+{
+	if (c > 64  && c < 92)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
