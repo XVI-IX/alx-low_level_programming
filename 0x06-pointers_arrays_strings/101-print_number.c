5dc0707087183713399bd3c2c9f3a1bc7eb82309
XVI-IX <oladoja14@gmail.com>
2022-03-25 09:33:47 +0000

    completed advanced tasks

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -11,21 +11,28 @@ void print_number(int n)
 
 	n /= 10;
 	copy = n;
+
 	if (ones < 0)
 	{
-		ones *= -1, copy *= -1, n *= -1;
+		ones *= -1;
+		copy *= -1;
+		n *= -1;
+
 		_putchar('-');
 	}
 	if (copy > 0)
 	{
 		while (copy / 10 != 0)
 		{
-			copy /= 10, size *= 10;
+			copy /= 10;
+			size *= 10;
 		}
 		while (size > 0)
 		{
 			nth = n / size;
+
 			_putchar('0' + nth);
+
 			n -= nth * size;
 			size /= 10;
 		}
